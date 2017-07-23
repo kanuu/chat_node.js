03c6276349e7bb44d8cfc2be80e920ea547b56d9
Taka <takaharu.kano@gmail.com>
2017-07-23 14:56:12 +0900

    入室処理とID表示を実装

--- index.js
+++ index.js
@@ -8,9 +8,34 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+	//接続時に振られた一意のIDをコンソールに表示
+	console.log('%s さんが接続しました。', socket.id);
+
+	//デフォルトのチャンネル
+  var channel = 'channel-a';
+
+  //接続時に自分以外の全員にIDを表示
+  socket.broadcast.emit('chat message', socket.id + 'さんが入室しました！', 'system');
+
+  //Roomを初期化
+  socket.join(channel);
+
+  //messageイベントで動く
+  //同じチャンネルの人にメッセージを送る
+  socket.on('message', function(msj) {
+    io.sockets.in(channel).emit('message', msg, socket.id);
   });
+
+
+  socket.on('chat message', function(msg){
+  	io.sockets.in(channel).emit('chat message', msg, socket.id);
+    // io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(e) {
+    console.log('%s さんが退室しました。', socket.id);
+  });
+
 });
 
 http.listen(PORT, () => {
